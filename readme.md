9cdf12a1856b0044d2d3e01b50753d554c9a4ed3
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-24 09:39:41 +0300

    providing app wide state in index.js

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # redux basics in react project
 
 1.npm i redux react-redux
+
+2. susikurti redux pgr faila
+
+3.aprasom actions ir state
+
+4. sukuriam store ir iseksportuojam
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
 
+const initialCounterState = { counter: 0 };
 // reducer f-ja
-const counterReducer = () => {};
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case 'UP':
+      return { counter: state.counter + 1 };
+    case 'DOWN':
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
 // store sukurimas
 const store = createStore(counterReducer);
+
+export default store;
